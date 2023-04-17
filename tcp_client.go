a43306a3f117360904b5c6a1f589af1dadd1a136
Simen <simenhol@uia.no>
2023-04-17 15:53:12 +0200

    "La til kryptertfunksjon"

--- tcp_client.go
+++ tcp_client.go
@@ -1,20 +1,25 @@
 package main
 
 import (
-	"net"
 	"log"
+	"net"
 	"os"
+
+	"github.com/SiMENhol/is105sem03/mycrypt"
 )
 
 func main() {
+
 	conn, err := net.Dial("tcp", "127.0.0.1:")
 	if err != nil {
 		log.Fatal(err)
 	}
-    
+	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
+	log.Println("Kryptert melding: ", string(kryptertMelding))
+	_, err = conn.Write([]byte(string(kryptertMelding)))
 	log.Println("os.Args[1] = ", os.Args[1])
 
- 	_, err = conn.Write([]byte(os.Args[1]))
+	_, err = conn.Write([]byte(os.Args[1]))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -22,7 +27,7 @@ func main() {
 	n, err := conn.Read(buf)
 	if err != nil {
 		log.Fatal(err)
-	} 
+	}
 	response := string(buf[:n])
 	log.Printf("reply from proxy: %s", response)
 }
