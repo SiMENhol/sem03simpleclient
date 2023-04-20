fa74f7e79580913c724b5b988c66c82f4619ec68
Simen <simenhol@uia.no>
2023-04-20 18:24:48 +0200

    asd

--- tcp_client.go
+++ tcp_client.go
@@ -18,9 +18,7 @@ func main() {
 	log.Println("Kryptert melding: ", string(kryptertMelding))
 	_, err = conn.Write([]byte(string(kryptertMelding)))
 	log.Println("os.Args[1] = ", os.Args[1])
-}
 
-/*
 	_, err = conn.Write([]byte(os.Args[1]))
 	if err != nil {
 		log.Fatal(err)
@@ -33,4 +31,3 @@ func main() {
 	response := string(buf[:n])
 	log.Printf("reply from proxy: %s", response)
 }
-*/
