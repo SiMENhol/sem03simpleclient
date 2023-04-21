a5e0c5ace19c263bf85ffb0a1479c6b69cea0601
Simen <simenhol@uia.no>
2023-04-21 12:19:53 +0200

    AsaSAsa

--- tcp_client.go
+++ tcp_client.go
@@ -9,6 +9,31 @@ import (
 )
 
 func main() {
+	conn, err := net.Dial("tcp", "172.17.0.2:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("os.Args[1] = ", os.Args[1])
+
+	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
+	log.Println("Kryptert melding: ", string(kryptertMelding))
+
+	_, err = conn.Write([]byte(string(kryptertMelding)))
+	if err != nil {
+		log.Fatal(err)
+	}
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	response := string(buf[:n])
+	log.Printf("reply from proxy: %s", response)
+}
+
+/*
+func main() {
 
 	conn, err := net.Dial("tcp", "172.17.0.2:8080")
 	if err != nil {
@@ -37,3 +62,4 @@ func main() {
 	log.Printf("reply from proxy: %s", response)
 
 }
+*/
