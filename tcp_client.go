05d6851bc053791fb04be5b9c7a911fa32c716e6
SIMENhol <simenhol@uia.no>
2023-04-21 09:03:47 +0200

    asd

--- tcp_client.go
+++ tcp_client.go
@@ -14,9 +14,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, +4)
-	log.Println("Kryptert melding: ", string(kryptertMelding))
-	_, err = conn.Write([]byte(string(kryptertMelding)))
+
 	log.Println("os.Args[1] = ", os.Args[1])
 
 	_, err = conn.Write([]byte(os.Args[1]))
@@ -28,6 +26,9 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
+	log.Println("Kryptert melding: ", string(kryptertMelding))
+	_, err = conn.Write([]byte(string(kryptertMelding)))
 
 	dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
 	log.Println("Dekryptert melding: ", string(dekryptertMelding))
