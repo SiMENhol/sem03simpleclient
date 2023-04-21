783186ef55227a1d5b61462ff45691f160ed734a
Simen <simenhol@uia.no>
2023-04-21 13:43:10 +0200

    Ferdigstile koden

--- tcp_client.go
+++ tcp_client.go
@@ -16,9 +16,9 @@ func main() {
 
 	log.Println("os.Args[1] = ", os.Args[1])
 
-	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
+	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4) //Dette kryptere meldingen
 
-	_, err = conn.Write([]byte(string(kryptertMelding)))
+	_, err = conn.Write([]byte(string(kryptertMelding))) //Dette sender den krypterte meldingen
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -27,9 +27,9 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	response := string(buf[:n])
-	log.Printf("reply from proxy: %s", response)
-	dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)+4)
-	log.Println("Dekrypter melding: ", string(dekryptertMelding))
+	response := string(buf[:n])                                                                                //Dette gjør om fra byte til string
+	log.Printf("reply from proxy: %s", response)                                                               //Printer response
+	dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)+4) //Meldingen som komme tilbake blir dekrypt her
+	log.Println("Dekrypter melding: ", string(dekryptertMelding))                                              //Printer den dekrypte meldingen
 
 }
