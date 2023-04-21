4577d3c165469b10a4aa37aa2862cfb984e89c5e
Simen <simenhol@uia.no>
2023-04-21 12:23:16 +0200

    AsaSA

--- tcp_client.go
+++ tcp_client.go
@@ -30,36 +30,14 @@ func main() {
 	}
 	response := string(buf[:n])
 	log.Printf("reply from proxy: %s", response)
+	dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+	log.Println("Dekryptert melding: ", string(dekryptertMelding))
 }
 
 /*
-func main() {
 
-	conn, err := net.Dial("tcp", "172.17.0.2:8080")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
-	log.Println("Kryptert melding: ", string(kryptertMelding))
-	_, err = conn.Write([]byte(string(kryptertMelding)))
-
-	log.Println("os.Args[1] = ", os.Args[1])
-
-	_, err = conn.Write([]byte(os.Args[1]))
-	if err != nil {
-		log.Fatal(err)
-	}
-	buf := make([]byte, 1024)
-	n, err := conn.Read(buf)
-	if err != nil {
-		log.Fatal(err)
-	}
 	dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
 	log.Println("Dekryptert melding: ", string(dekryptertMelding))
-
-	response := string(buf[:n])
-	log.Printf("reply from proxy: %s", response)
 
 }
 */
